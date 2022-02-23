3ec142b15edfffd0c6dbe532d5faeadd49ca053a
Mendel Jacks <mendeljacks@gmail.com>
2022-02-23 12:52:13 -0500

    export rating

--- src/types.ts
+++ src/types.ts
@@ -19,8 +19,9 @@ export type ContentDetailParams = {
     allRoomTypes?: boolean
     links?: string //WD,WS,AD,WEB,API
 }
+export type Rating = '1.0' | '1.5' | '2.0' | '2.5' | '3.0' | '3.5' | '4.0' | '4.5' | '5.0'
 export type FilteringParams = {
-    minStarRating?: '1.0' | '1.5' | '2.0' | '2.5' | '3.0' | '3.5' | '4.0' | '4.5' | '5.0'
+    minStarRating?: Rating
     maxStarRating?: string
     limit?: number
     queryText?: string
